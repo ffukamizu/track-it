c679147221aab3a1037f0557da9b0f472aab6724
ffukamizu <f.fukamizu@live.com>
2023-06-05 02:58:34 -0300

    bug fixes

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -4,8 +4,10 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const storedToken = localStorage.getItem("token");
+    const storedLogInData = localStorage.getItem("logInData");
+
     const [token, setToken] = useState(storedToken || null);
-    const [logInData, setLogInData] = useState(null);
+    const [logInData, setLogInData] = useState(storedLogInData || null);
 
     useEffect(() => {
         if (token) {
@@ -14,6 +16,14 @@ export const AuthProvider = ({ children }) => {
             localStorage.removeItem("token");
         }
     }, [token]);
+
+    useEffect(() => {
+        if (logInData) {
+            localStorage.setItem("logInData", logInData);
+        } else {
+            localStorage.removeItem("logInData");
+        }
+    }, [logInData]);
 
     return <AuthContext.Provider value={{ token, setToken, logInData, setLogInData }}>{children}</AuthContext.Provider>;
 };
