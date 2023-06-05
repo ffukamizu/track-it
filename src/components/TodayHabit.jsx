2f5b47744b2c00f04991429e74a16735ab55c792
ffukamizu <f.fukamizu@live.com>
2023-06-05 16:14:38 -0300

    implemented conditional coloring to today

--- src/components/TodayHabit.jsx
+++ src/components/TodayHabit.jsx
@@ -7,7 +7,6 @@ import { AuthContext } from "./../AuthContext";
 
 export default function TodayHabit(props) {
     const { token } = useContext(AuthContext);
-    let { habitsDone, setHabitsDone } = useContext(AuthContext);
 
     const [isCompleted, setIsCompleted] = useState(props.content.done);
 
@@ -43,11 +42,19 @@ export default function TodayHabit(props) {
     }
 
     return (
-        <HabitContainer data-test="today-habit-container" >
+        <HabitContainer data-test="today-habit-container">
             <div>
                 <HabitContent data-test="today-habit-name">{props.content.name}</HabitContent>
-                <CurrentStreak data-test="today-habit-sequence">Current streak: {props.content.currentSequence}</CurrentStreak>
-                <AllTimeStreak data-test="today-habit-record">Highest sequence: {props.content.highestSequence}</AllTimeStreak>
+                <CurrentStreak data-test="today-habit-sequence" isCompleted={isCompleted}>
+                    Current streak: {props.content.currentSequence}
+                </CurrentStreak>
+                <AllTimeStreak
+                    data-test="today-habit-record"
+                    isCompleted={isCompleted}
+                    currentSequence={props.content.currentSequence}
+                    highestSequence={props.content.highestSequence}>
+                    Highest sequence: {props.content.highestSequence}
+                </AllTimeStreak>
             </div>
             <div>
                 <Check data-test="today-habit-check-btn" isCompleted={isCompleted} onClick={() => toggleHabit(props.content.id)}>
@@ -89,7 +96,7 @@ const CurrentStreak = styled.p`
     font-weight: 400;
     font-size: 13px;
     line-height: 16px;
-    color: #666666;
+    color: ${(props) => (props.isCompleted ? "#8FC549" : "#666666")};
 `;
 
 const AllTimeStreak = styled.p`
@@ -98,7 +105,7 @@ const AllTimeStreak = styled.p`
     font-weight: 400;
     font-size: 13px;
     line-height: 16px;
-    color: #666666;
+    color: ${(props) => (props.isCompleted && props.currentSequence >= props.highestSequence && props.currentSequence > 0 ? "#8FC549" : "#666666")};
 `;
 
 const Check = styled.div`
